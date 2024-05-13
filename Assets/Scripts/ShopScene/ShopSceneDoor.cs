2e6efdb7d792d1c2b7f33dda837060fbe8cbfd2d
RUBEN COLLADO DIAZ <e.rcolladod@ies-sabadell.cat>
2024-05-13 13:44:56 +0200

    Cambios jluis

--- Assets/Scripts/ShopScene/ShopSceneDoor.cs
+++ Assets/Scripts/ShopScene/ShopSceneDoor.cs
@@ -5,14 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class ShopSceneDoor : MonoBehaviour
 {
+    public Transform spawnPoint;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
             if (Input.GetKey(KeyCode.DownArrow) || (Input.GetKey(KeyCode.S)))
             {
+                PlayerPrefs.SetFloat("SpawnPointX", -7.48f);
+                PlayerPrefs.SetFloat("SpawnPointY", 3.06f);
+                PlayerPrefs.SetFloat("SpawnPointZ", 0f);
+
                 SceneManager.LoadScene("StartScene");
             }
+
         }
     }
 
